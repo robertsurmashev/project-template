ec97122c2d50fbd5a25abd2059a84c45d80906cc
andriy-sdet <andriy.dyhal@gmail.com>
2025-09-12 12:02:45 -0500

    Changes made

--- src/test/java/com/myFleet/pages/BasePage.java
+++ src/test/java/com/myFleet/pages/BasePage.java
@@ -38,14 +38,6 @@ public class BasePage {
     @FindBy(linkText = "My User")
     public WebElement myUser;
 
-    @FindBy(xpath = "//a[.='Learn how to use this space']")
-    public WebElement pianoLink;
-
-    @FindBy(xpath = "//div[@class='clearfix']/h3")
-    public WebElement linkText;
-
-    @FindBy(xpath = "//img[@src='/bundles/oronavigation/images/pinbar-location.jpg']")
-    public WebElement pinbarImg;
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
--- src/test/java/com/myFleet/step_definitions/B40GRP02_131_StepDefs.java
+++ src/test/java/com/myFleet/step_definitions/B40GRP02_131_StepDefs.java
@@ -2,6 +2,7 @@ package com.myFleet.step_definitions;
 
 import com.myFleet.pages.BasePage;
 import com.myFleet.pages.LoginPage;
+import com.myFleet.pages.UserPageAD;
 import com.myFleet.utilities.BrowserUtils;
 import com.myFleet.utilities.Driver;
 import io.cucumber.java.en.*;
@@ -10,22 +11,22 @@ import org.junit.Assert;
 public class B40GRP02_131_StepDefs {
 
     LoginPage loginPage = new LoginPage();
-    BasePage basePage = new BasePage();
+    UserPageAD userPageAD = new UserPageAD();
 
     @Given("when user click on the link")
     public void when_user_click_on_the_link() {
-        basePage.pianoLink.click();
+        userPageAD.pianoLink.click();
     }
     @Then("user can see How to use pinbar text")
     public void user_can_see_how_to_use_pinbar_text() {
         String expectedText = "How To Use Pinbar";
-        String actualText = basePage.linkText.getText();
+        String actualText = userPageAD.linkText.getText();
         Assert.assertEquals(expectedText, actualText);
     }
 
     @Then("pinbar image is displayed")
     public void pinbar_image_is_displayed() {
-        String actualSrc = basePage.pinbarImg.getAttribute("src");
+        String actualSrc = userPageAD.pinbarImg.getAttribute("src");
         String expectedSrc = "https://qa3.vytrack.com/bundles/oronavigation/images/pinbar-location.jpg";
         Assert.assertEquals(expectedSrc, actualSrc);
     }
